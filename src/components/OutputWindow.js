4774692a50d1fd129f4b83f50cfc80b343c47cac
zchen225 <zac41@pitt.edu>
2024-12-05 12:20:00 -0500

    Implemented dashboard component and header

--- src/components/OutputWindow.js
+++ src/components/OutputWindow.js
@@ -7,13 +7,14 @@ const OutputWindow = ({ outputDetails }) => {
     if (statusId === 6) {
       // compilation error
       return (
-        <pre className="px-2 py-1 font-normal text-xs text-red-500">
+        <pre className="px-2 py-1 text-xs font-normal text-red-500">
           {atob(outputDetails?.compile_output)}
         </pre>
       );
     } else if (statusId === 3) {
+      console.log(atob(outputDetails.stdout));
       return (
-        <pre className="px-2 py-1 font-normal text-xs text-green-500">
+        <pre className="px-2 py-1 text-xs font-normal text-green-500">
           {atob(outputDetails.stdout) !== null
             ? `${atob(outputDetails.stdout)}`
             : null}
@@ -21,13 +22,13 @@ const OutputWindow = ({ outputDetails }) => {
       );
     } else if (statusId === 5) {
       return (
-        <pre className="px-2 py-1 font-normal text-xs text-red-500">
+        <pre className="px-2 py-1 text-xs font-normal text-red-500">
           {`Time Limit Exceeded`}
         </pre>
       );
     } else {
       return (
-        <pre className="px-2 py-1 font-normal text-xs text-red-500">
+        <pre className="px-2 py-1 text-xs font-normal text-red-500">
           {atob(outputDetails?.stderr)}
         </pre>
       );
@@ -35,10 +36,10 @@ const OutputWindow = ({ outputDetails }) => {
   };
   return (
     <>
-      <h1 className="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-slate-700 mb-2">
-        Output
+      <h1 className="mb-2 text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-slate-900 to-slate-700">
+        Output:
       </h1>
-      <div className="w-full h-56 bg-[#1e293b] rounded-md text-white font-normal text-sm overflow-y-auto">
+      <div className="w-full h-56 bg-[#010306] rounded-md text-white font-normal text-sm overflow-y-auto">
         {outputDetails ? <>{getOutput()}</> : null}
       </div>
     </>
